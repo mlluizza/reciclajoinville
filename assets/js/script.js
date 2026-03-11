26a998e5426c0882671afda8db0021c250a80a7c
Maria Luiza Silva e Silva <mlluizz918@gmail.com>
2026-03-11 00:02:58 -0300

    Ulltimas correções

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.getElementById('fileInput').addEventListener('change', function() {
-    const fileList = this.files[0];
+    const file = this.files[0];
     var fileName = file ? file.name : 'Nenhum arquivo selecionado';
     document.getElementById('fileName').textContent = 'Select file: ' + fileName;
 });
